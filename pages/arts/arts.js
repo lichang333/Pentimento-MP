57e4d42fd8b51ccef37443f8be5a48e32ddd3ff1
Kevin Tang <ktang06@gmail.com>
2018-12-04 16:44:46 +0800

    Ellery pushed master, so my code was deleted. Thankfully I had a back, also his idea. So it's all good

--- pages/arts/arts.js
+++ pages/arts/arts.js
@@ -1,66 +1,69 @@
-// pages/arts/arts.js
+// pages/workspaces/arts.js
+let app = getApp()
+
 Page({
 
-  /**
-   * Page initial data
-   */
   data: {
 
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
   onLoad: function (options) {
+    let page = this;
+
+    //Request API to get workspace
+    wx.request({
+      url: "https://workspace-api333.herokuapp.com/api/v1/workspaces",
+      method: 'GET',
+      success(res) {
+        const workspaces = res.data;
+        console.log(3242342, workspaces);
+        page.setData({
+          workspaces: workspaces
+        });
+
+        wx.hideToast();
+      }
+    });
 
   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
   onReady: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page show
-   */
   onShow: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page hide
-   */
   onHide: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page unload
-   */
   onUnload: function () {
 
   },
 
-  /**
-   * Page event handler function--Called when user drop down
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * Called when page reach bottom
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * Called when user click on the top right corner to share
-   */
   onShareAppMessage: function () {
 
+  },
+  showWorkspace(e) {
+    const workspaceId = e.currentTarget.dataset.id;
+
+    wx.navigateTo({
+      url: `/pages/space/space?id=${workspaceId}`,
+    });
+  },
+  goToHomePage: function () {
+    wx.navigateTo({
+      url: '/pages/index/index'
+    })
   }
 })
