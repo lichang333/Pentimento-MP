12c7e6ad297454793444b14bf41ef77bc6cbc81c
Chang Li <lichang@oregonstate.edu>
2018-12-05 11:59:28 +0800

    merged the work together.
    without conflit

--- pages/arts/arts.js
+++ pages/arts/arts.js
@@ -1,66 +1,64 @@
-// pages/arts/arts.js
+// pages/workspaces/arts.js
+let app = getApp()
+
 Page({
 
-  /**
-   * Page initial data
-   */
   data: {
 
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
   onLoad: function (options) {
+    let page = this;
+
+    //Request API to get workspace
+    wx.request({
+      url: "http://pentimento-mp.ellerystars.xyz/api/mock_paintings.json",
+      method: 'GET',
+      success(res) {
+        const artworks = res.data;
+        console.log(3242342, artworks);
+        page.setData({
+          artworks
+        });
+
+        wx.hideToast();
+      }
+    });
 
   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
   onReady: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page show
-   */
   onShow: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page hide
-   */
   onHide: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page unload
-   */
   onUnload: function () {
 
   },
 
-  /**
-   * Page event handler function--Called when user drop down
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * Called when page reach bottom
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * Called when user click on the top right corner to share
-   */
   onShareAppMessage: function () {
 
+  },
+  showWorkspace(e) {
+    const workspaceId = e.currentTarget.dataset.id;
+
+    wx.navigateTo({
+      url: `/pages/art_details/art_details?id=${workspaceId}`,
+    });
   }
 })
